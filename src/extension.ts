0dcf65a02cc2de4c1b62e2c6d8d4d7efa2954a4d
badetitou <badetitou@gmail.com>
2021-04-01 08:45:39 +0200

    begin work on tree

--- src/extension.ts
+++ src/extension.ts
@@ -16,6 +16,7 @@ import * as child_process from 'child_process';
 import * as lc from 'vscode-languageclient';
 import { activateDebug } from './activateDebug'
 import { DebugAdapterFactory } from './debugFactory'
+import { PharoImageExplorer } from './treeProvider/imageExplorer';
 
 export let client: LanguageClient;
 let socket: net.Socket;
@@ -40,6 +41,8 @@ export async function activate(context: ExtensionContext) {
 	
 		// Create new command
 		createCommands(context);
+
+		new PharoImageExplorer(context);
 
 		// Create debugguer
 		let factory = new DebugAdapterFactory();
---
+++ src/treeProvider/imageExplorer.ts
@@ -0,0 +1,49 @@
+import * as vscode from 'vscode';
+import { client } from '../extension';
+
+export interface PharoNode {
+
+	resource: vscode.Uri;
+	isDirectory: boolean;
+
+}
+
+export class PharoDataProvider implements vscode.TreeDataProvider<PharoNode>, vscode.TextDocumentContentProvider {
+
+	private _onDidChangeTreeData: vscode.EventEmitter<any> = new vscode.EventEmitter<any>();
+	readonly onDidChangeTreeData: vscode.Event<any> = this._onDidChangeTreeData.event;
+
+	constructor() { }
+
+	public refresh(): any {
+		this._onDidChangeTreeData.fire(undefined);
+	}
+
+
+	public getTreeItem(element: PharoNode): vscode.TreeItem {
+		return {
+			resourceUri: element.resource,
+			collapsibleState: element.isDirectory ? vscode.TreeItemCollapsibleState.Collapsed : void 0
+		};
+	}
+
+	public getChildren(element?: PharoNode): PharoNode[] | Thenable<PharoNode[]>  {
+		if (element === undefined) {
+			return client.sendRequest('pls:packages', {}).then((result: Array<string>) => {
+				return result.sort().map((item) => { return { resource: vscode.Uri.parse(item), isDirectory: true }; })
+			});
+		}
+		return [{ resource: vscode.Uri.parse('test'), isDirectory: true }];
+	}
+
+	public provideTextDocumentContent(uri: vscode.Uri, token: vscode.CancellationToken): vscode.ProviderResult<string> {
+		return 'hello'
+	}
+}
+
+export class PharoImageExplorer {
+	constructor(context: vscode.ExtensionContext) {
+		const pharoDataProvider = new PharoDataProvider();
+		context.subscriptions.push(vscode.window.createTreeView('pharoImage', { treeDataProvider: pharoDataProvider }));
+	}
+}
