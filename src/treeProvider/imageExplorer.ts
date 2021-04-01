ae4ac137ed30681e276415f425a17948113dad40
badetitou <badetitou@gmail.com>
2021-04-01 14:00:22 +0200

    prepare tree view inside ?

--- src/treeProvider/imageExplorer.ts
+++ src/treeProvider/imageExplorer.ts
@@ -2,10 +2,9 @@ import * as vscode from 'vscode';
 import { client } from '../extension';
 
 export interface PharoNode {
-
+	name: string;
 	resource: vscode.Uri;
 	isDirectory: boolean;
-
 }
 
 export class PharoDataProvider implements vscode.TreeDataProvider<PharoNode>, vscode.TextDocumentContentProvider {
@@ -23,7 +22,12 @@ export class PharoDataProvider implements vscode.TreeDataProvider<PharoNode>, vs
 	public getTreeItem(element: PharoNode): vscode.TreeItem {
 		return {
 			resourceUri: element.resource,
-			collapsibleState: element.isDirectory ? vscode.TreeItemCollapsibleState.Collapsed : void 0
+			collapsibleState: element.isDirectory ? vscode.TreeItemCollapsibleState.Collapsed : void 0,
+			command: element.isDirectory ? void 0 : {
+				command: 'pharoImage.openClass',
+				arguments: [element.resource],
+				title: 'Open Pharo Class'
+			}
 		};
 	}
 
@@ -31,21 +35,41 @@ export class PharoDataProvider implements vscode.TreeDataProvider<PharoNode>, vs
 		if (element === undefined) {
 			return client.onReady().then(() =>
 				client.sendRequest('pls:packages', {}).then((result: Array<string>) => {
-					return result.sort().map((item) => { return { resource: vscode.Uri.parse(item), isDirectory: true }; })
+					return result.sort().map((item) => { return { name: item, resource: vscode.Uri.parse('pharoImage:' + item, true), isDirectory: true }; })
 				})
 			)
 		}
-		return [{ resource: vscode.Uri.parse('test'), isDirectory: true }];
+		return client.onReady().then(() =>
+			client.sendRequest('pls:classes', { package: element.name }).then((result: Array<string>) => {
+				return result.sort().map((item) => {
+					return {
+						name: item,
+						resource: vscode.Uri.parse('pharoImage:' + item, true),
+						isDirectory: false
+					};
+				})
+			})
+		)
 	}
 
 	public provideTextDocumentContent(uri: vscode.Uri, token: vscode.CancellationToken): vscode.ProviderResult<string> {
-		return 'hello'
+		return client.onReady().then(() =>{
+			return client.sendRequest('pls:classContent', { class: uri.path }).then((result: string) => result);
+		});
 	}
 }
 
 export class PharoImageExplorer {
 	constructor(context: vscode.ExtensionContext) {
 		const pharoDataProvider = new PharoDataProvider();
-		context.subscriptions.push(vscode.window.createTreeView('pharoImage', { treeDataProvider: pharoDataProvider }));
+		context.subscriptions.push(vscode.workspace.registerTextDocumentContentProvider('pharoImage', pharoDataProvider));
+		vscode.window.createTreeView('pharoImage', { treeDataProvider: pharoDataProvider });
+
+		vscode.commands.registerCommand('pharoImage.openClass', resource => this.openResource(resource));
+
+	}
+
+	private openResource(resource: vscode.Uri): void {
+		vscode.window.showTextDocument(resource);
 	}
 }
